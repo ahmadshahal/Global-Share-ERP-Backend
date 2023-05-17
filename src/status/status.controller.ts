fab3bc63613f07f6bf852cd06a64db31417daa44
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2023-05-17 10:00:05 +0300

    added comment-crud operations

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -8,11 +8,14 @@ import {
     Param,
     ParseIntPipe,
     Post,
+    UseGuards,
 } from '@nestjs/common';
 import { StatusService } from './status.service';
 import { CreateStatusDto } from './dto/create-status.dto';
 import { UpdateStatusDto } from './dto/update-status.dto';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
+@UseGuards(JwtGuard)
 @Controller('status')
 export class StatusController {
     constructor(private statusService: StatusService) {}
