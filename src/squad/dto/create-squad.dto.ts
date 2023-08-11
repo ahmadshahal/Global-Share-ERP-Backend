407cdf830a83d6f81e00e5c01b9ded7a4a642991
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2023-08-11 21:23:25 +0300

    Validation Fixes

--- src/squad/dto/create-squad.dto.ts
+++ src/squad/dto/create-squad.dto.ts
@@ -1,16 +1,19 @@
-import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateSquadDto {
+    @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(50)
     name: string;
 
+    @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(50)
     gsName: string;
 
+    @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(250)
